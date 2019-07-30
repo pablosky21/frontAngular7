e6f61c4b9ae51ad0bbc3e1ffb4fd1aabb1e6a202
pablo lincoleo <plsalgado@gmail.com>
2019-07-30 18:01:41 -0400

    [PLS] se suben cambios en el front

--- pabloskyfront/src/app/componentes/tipo-campo/tipo-campo.component.ts
+++ pabloskyfront/src/app/componentes/tipo-campo/tipo-campo.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit }from '@angular/core';
+import {ListtipoCampoService }from './../../services/listtipo-campo.service';
+import {Tipocampo }from './../../tipocampo';
 
 @Component( {
 selector:'app-tipo-campo',
@@ -10,7 +12,7 @@ nombre:string;
 apellido:string;
 edad:number;
 
-constructor() {
+constructor(private service:ListtipoCampoService) {
 this.nombre = "pablo";
 this.apellido = "lincoleo";
 this.edad = 33;
@@ -19,4 +21,11 @@ this.edad = 33;
 ngOnInit() {
 }
 
+addProduct() {
+const tipocampo:Tipocampo = new Tipocampo();
+this.service.addProduct(Tipocampo).subscribe((result) =>  {
+}, (err) =>  {
+console.log(err);
+});
+}
 }
--- pabloskyfront/src/app/services/listtipo-campo.service.ts
+++ pabloskyfront/src/app/services/listtipo-campo.service.ts
@@ -1,5 +1,5 @@
 import {Injectable }from '@angular/core';
-import {HttpClient }from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpErrorResponse }from '@angular/common/http';
 import {Observable, of }from 'rxjs';
 import {map, catchError, tap }from 'rxjs/operators';
 
@@ -10,7 +10,11 @@ providedIn:'root'
 })
 export class ListtipoCampoService {
 
+
+
+
 constructor(protected http:HttpClient) {}
+
 getTipos() {
 return this.http.get("http://localhost:8080/getPablosky");
 }
@@ -19,9 +23,35 @@ getProducts():Observable < any >  {
 return this.http.get("http://localhost:8080/getPablosky").pipe(
 map(this.extractData));
 }
+
+addProduct (product):Observable < any >  {
+const httpOptions =  {
+headers:new HttpHeaders( {
+'Content-Type':'application/json'
+})
+};
+console.log(product);
+return this.http.post < any > ('http://localhost:8080/insertPablosky', JSON.stringify(product), httpOptions).pipe(
+tap((product) => console.log(`added product w/ id = $ {product.id}`)),
+catchError(this.handleError < any > ('addProduct'))
+);
+}
 private extractData(res:Response) {
 let body = res;
 return body ||  {};
 }
 
+private handleError < T > (operation = 'operation', result?:T) {
+return (error:any):Observable < T >  =>  {
+
+// TODO: send the error to remote logging infrastructure
+console.error(error); // log to console instead
+
+// TODO: better job of transforming error for user consumption
+console.log(`$ {operation}failed:$ {error.message}`);
+
+// Let the app keep running by returning an empty result.
+return of(result as T);
+};
+}
 }
